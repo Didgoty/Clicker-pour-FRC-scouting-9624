6084180eba68fe575850c2516f9353f8393f1ac6
Didgoty <dieg.ludbas1@gmail.com>
2026-01-11 21:43:49 -0500

    embarrassing error

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let clicks = 0;
 const counter = document.getElementById("counter")
 document.addEventListener("keydown", (e) => {
-    if (e.code !== "space") return;
+    if (e.code !== "Space") return;
     clicks += 1;
     counter.textContent = clicks;
 });
